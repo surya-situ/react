43f874db6af4c5cd89f87e3aaa087bc52cd2035e
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-02-18 13:42:52 +0530

    new cards added

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -8,9 +8,9 @@ const RestaurantCard = ({name, cuisines, lastMileTravel, cloudinaryImageId}) => 
     return (
         <div className='card'>
             <img src={IMG_CDN_URL + cloudinaryImageId}  />
-            <h2>{name}</h2>
-            <h4>{cuisines.join(', ')}</h4>
-            <h5>{Math.trunc(lastMileTravel)} km away from your home</h5>
+            <h3>{name}</h3>
+            <p>{cuisines.join(', ')}</p>
+            <p>{Math.trunc(lastMileTravel)} km away from your home</p>
         </div>
     )
 }
