7498168dddf7e5bceeacd2da1774d63bb4a23059
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-02-18 13:41:28 +0530

    making the search box functionable

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,19 @@ import { restaurantList} from '../Config';
 import RestaurantCard from './RestaurantCard';
 
 
+function filterData(searchText, restaurant) {
+    const filteringData =  restaurant.filter((restaurant) => restaurant.data.name.includes(searchText));
+
+    return filteringData;
+}
+
+
 const Body = () => {
 
-    const [searchText, setSearchText] = useState('');
+    // importing restaurant list data
+    const [restaurant, setRestaurant] = useState(restaurantList);
 
+    const [searchText, setSearchText] = useState('');
 
     return (
         <>
@@ -20,15 +29,32 @@ const Body = () => {
                     value= {searchText}
                     onChange={(event) => {setSearchText(event.target.value)}}
                 />
-                <button className='search-btn'>search</button>
+                <button 
+                    className='search-btn' 
+                    onClick={() => 
+                        {
+                            // need to filter data
+                           const data =  filterData(searchText, restaurant);
+
+                            // update the state- restaurant
+                            setRestaurant(data);
+                            // setting input value to empty after button click
+                            setSearchText('');
+
+                        }
+                    }
+                    
+                    
+                >search
+                </button>
             </div>
 
             {/* body */}
             <div className='body'>
                 {
-                    restaurantList.map((restaurant) => {
+                    restaurant.map((swiggyData) => {
                         return (
-                            <RestaurantCard key={restaurant.data.id} {...restaurant.data} />
+                            <RestaurantCard key={swiggyData.data.id} {...swiggyData.data} />
                         )
                     })
                 }
