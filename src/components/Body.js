525600f2dfe8d7beeb95a81af0034ce19501d273
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-02-22 11:06:10 +0530

    correcting the api error

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,9 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import '../App.css';
 import { restaurantList} from '../Config';
 import RestaurantCard from './RestaurantCard';
 
-
+// filtering data through search box:
 function filterData(searchText, restaurant) {
     const filteringData =  restaurant.filter((restaurant) => restaurant.data.name.includes(searchText));
 
@@ -11,12 +11,34 @@ function filterData(searchText, restaurant) {
 }
 
 
+
 const Body = () => {
 
     // importing restaurant list data
     const [restaurant, setRestaurant] = useState(restaurantList);
-
     const [searchText, setSearchText] = useState('');
+
+
+    useEffect(() => {
+        // API call
+        getRestaurant();
+    }, []);
+
+    const getRestaurant = async () => {
+    
+        try {   
+            const data = await fetch(`https://www.swiggy.com/dapi/restaurants/list/v5?lat=20.2960587&lng=85.8245398&page_type=DESKTOP_WEB_LISTING`);
+
+            const result = await data.json();
+
+            // optional chancing
+            setRestaurant(result.data?.cards[2]?.data?.data?.cards);
+
+        } catch(error) {
+            console.log('error in fetching')
+        }
+       
+    }
 
     return (
         <>
@@ -36,25 +58,26 @@ const Body = () => {
                             // need to filter data
                            const data =  filterData(searchText, restaurant);
 
+                           setRestaurant(data)
+
                             // update the state- restaurant
-                            setRestaurant(data);
+                            
                             // setting input value to empty after button click
                             setSearchText('');
 
                         }
-                    }
-                    
-                    
-                >search
-                </button>
+                    }  
+                > search </button>
             </div>
 
             {/* body */}
             <div className='body'>
                 {
-                    restaurant.map((swiggyData) => {
-                        return (
+
+                    restaurant && restaurant.map((swiggyData) => {
+                        return(
                             <RestaurantCard key={swiggyData.data.id} {...swiggyData.data} />
+                          
                         )
                     })
                 }
