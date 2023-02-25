2684ba0269bca851c519ed937f837143256150a7
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-02-25 19:43:57 +0530

    log-in log-out button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../App.css'
 
 import Title from './Title';
 
+
 const Header = () => {
 
+  const[loggedIn, setLoggedIn] = useState(true);
 
   return (
     <div className='header'>
@@ -17,9 +19,15 @@ const Header = () => {
                 <li>Contact us</li>
                 <li>Cart</li>
             </ul>
+            {
+
+              loggedIn ? ( <button onClick={() => setLoggedIn(false)}>log out</button> ) : ( <button  onClick={() => setLoggedIn(true)}>log in</button> )
+            
+            }
+            
         </div>
     </div>
   )
 }
 
-export default Header
+export default Header;
