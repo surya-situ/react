932d27cd5f5c0a796815b4c8829c2a191694039b
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-03-10 16:02:19 +0530

    modifing Links

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import '../App.css';
 
+import Logo from '../assets/img/food-logo.webp'
+
 const Title = () => {
   return (
     <a>
         <img 
             className= 'logo' 
             
-            src="https://yt3.googleusercontent.com/ytc/AL5GRJXudT76175T4x4n7eslWM1YkgNLHDSSqfXGoadl=s900-c-k-c0x00ffffff-no-rj" 
-
+            src={Logo}
             alt="Restaurant logo" 
         />
        
