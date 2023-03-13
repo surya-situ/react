a0d7c92120e3fd9a51a71f893eda8d5833bfecac
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-03-13 15:15:11 +0530

    adding profile section

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import ProfileClass from './ProfileClass';
+// import ProfileFunction from './Profile';
+
+
+let About = () => {
+
+    console.log('parent render.');
+    return (
+        <div>
+            <h1>About us page</h1>
+            <p>This is the Namaste react live course <span style={{color: 'white', backgroundColor:'green', padding: '2px 15px', fontSize:'50px', borderRadius:'10px'}}>+</span> +</p>
+
+            {/* <ProfileFunction name={'SuryaKanta Das'} /> */}
+            <ProfileClass name ={'surya Class components'} />
+        </div>
+    )
+};
+
+export default About;
