71a239ef6e37c19a7e7c6538b9cc62fb25f91877
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-03-14 12:17:11 +0530

    displying error message

---
+++ src/components/Error.js
@@ -0,0 +1,16 @@
+import React,{useRouteError} from 'react-router-dom';
+
+const Error = () => {
+    const err = useRouteError();
+    console.log(err);
+  return (
+    <div>
+        <h1>Oops!</h1>
+        <p>Something went wrong.😑</p>
+        <p>{err.status + " : "+ err.statusText}</p>
+        <p>{`${err.status} : ${err.statusText}`}</p>
+    </div>
+  )
+}
+
+export default Error;
