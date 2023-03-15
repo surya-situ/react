298bf68096b4066618fe990e0424161034ef6d3b
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-03-15 13:35:46 +0530

    adding new className

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React,{useState} from 'react';
-import '../App.css'
+import {Link} from 'react-router-dom';
+import useOnline from '../utils/useOnline';
 
 import Title from './Title';
 
@@ -9,16 +10,22 @@ const Header = () => {
   const[loggedIn, setLoggedIn] = useState(true);
 
   return (
-    <div className='header'>
+    <div className='flex justify-between bg-red-50 sha'>
         <Title />
 
-        <div className='nav-items'>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact us</li>
-                <li>Cart</li>
+        <div className='flex'>
+            <ul className='flex px-10 py-8'>
+
+              <li className='px-3'><Link to='/'>Home</Link></li>
+              <li className='px-3'><Link to='/about'>About</Link></li>
+              <li className='px-3'> <Link to="/contact">Contact</Link></li>
+              <li className='px-3'> <Link to="/instamart">Instamart</Link></li>
+              <li>Cart</li>
+
             </ul>
+
+            <h1>{useOnline ? '✅' : '⛔'}</h1>
+
             {
 
               loggedIn ? ( <button onClick={() => setLoggedIn(false)}>log out</button> ) : ( <button  onClick={() => setLoggedIn(true)}>log in</button> )
