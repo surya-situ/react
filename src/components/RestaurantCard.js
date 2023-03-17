c3d50076ebb38d9501a0485461cc5aebd9e40455
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-03-17 10:51:41 +0530

    displaying data in cards

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import '../App.css';
-
+import React,{useContext} from 'react';
 import { IMG_CDN_URL } from '../Config';
+import UserContext from '../utils/UserContext';
+
 
 
 const RestaurantCard = ({name, cuisines, lastMileTravel, cloudinaryImageId}) => {
+
+    const {user} = useContext(UserContext);
     return (
-        <div className='card'>
+        <div className='w-[200px] p-2 m-2 shadow-lg bg-red-100'>
             <img src={IMG_CDN_URL + cloudinaryImageId} />
-            <h3>{name}</h3>
+            <h3 className='font-bold text-l'>{name}</h3>
             <p>{cuisines.join(', ')}</p>
             <p>{Math.trunc(lastMileTravel)} km away from your home</p>
+            <span>{user.email}</span>
         </div>
     )
 }
