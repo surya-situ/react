efb80e01cf589fd9653682e6166bacae2f4e8f52
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-03-28 19:21:04 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import UserContext from '../utils/UserContext'
 
 const Footer = () => {
+
+    const {user} = useContext(UserContext)
+
     return (
-        <h4>Footer</h4>
+        <div className='bg-yellow-500 flex justify-center align-middle p-5'>
+
+            <h4>Footer</h4>
+            <h3>{user.name}</h3>
+        </div>
     )
     
 }
