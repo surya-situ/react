0c1cadf7b8b73af03b37b9d029fcfb80a3688538
Suryakanta Das <116926717+surya-situ@users.noreply.github.com>
2023-03-31 22:35:22 +0530

    user data

--- src/utils/UserContext.js
+++ src/utils/UserContext.js
@@ -1,8 +1,10 @@
 import {createContext} from 'react';
 
 const UserContext = createContext({
-    name:'Dummy Name',
-    email:'dummy@gmail.com'
+    user: {
+        name:'Dummy Name',
+        email:'dummy@gmail.com'
+    }
 })
 
 export default UserContext;
